a1721dbe835971d810236621a51fe61463037064
PRF <462316190@qq.com>
2021-03-23 11:17:49 +0800

    测试提交
    1测试提交新文件

---
+++ PRFTools/RFTest.h
@@ -0,0 +1,19 @@
+//
+//  RFTest.h
+//  MyTools
+//
+//  Created by 庞日富 on 2021/3/23.
+//  Copyright © 2021 庞日富. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RFTest : NSObject
+
++(void)sayhello;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PRFTools/RFTest.m
@@ -0,0 +1,16 @@
+//
+//  RFTest.m
+//  MyTools
+//
+//  Created by 庞日富 on 2021/3/23.
+//  Copyright © 2021 庞日富. All rights reserved.
+//
+
+#import "RFTest.h"
+
+@implementation RFTest
+
++(void)sayhello{
+    NSLog(@"hello");
+}
+@end
